b186f8fee789be31387d0f0be482f9d20a9c0faa
Max <maxtarasevich@gmail.com>
2021-07-16 09:45:00 +0300

    add jquery ui  add select menu, button

--- script/main.js
+++ script/main.js
@@ -1,1 +1,33 @@
-console.log(`1111111`)
+
+
+$(document).ready(function(){
+
+    $( "#pizza" ).selectmenu({
+        change: function( ) {
+            $( "#size" ).removeAttr( "disabled" )
+            $( "#size" ).selectmenu( "refresh" );
+        }
+      });
+
+    $( "#size" ).selectmenu({
+        change: function( ) {
+            $( "#sauce" ).removeAttr( "disabled" )
+            $( "#sauce" ).selectmenu( "refresh" );
+        }
+      });
+
+    $( "#sauce" ).selectmenu({
+        change: function( ) {
+            $( "input[type=submit]" ).removeAttr( "disabled" )
+            $( "input[type=submit]" ).button( "refresh" );
+        }
+      });
+
+    $( "input[type=submit]" ).button();
+    
+    $(".form").on("submit", (event) => {
+        event.preventDefault();
+    })
+   
+  
+  });
